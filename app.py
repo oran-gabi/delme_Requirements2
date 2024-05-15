728a7194cc6b866b6709742496db8b35f893396c
oran-gabi <orangabai@gmail.com>
2024-05-15 15:21:01 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from icecream import ic
+from math_1 import mult,sum
+
+#this is my first application
+if __name__ == '__main__':
+ ic(mult(2,4))
+ ic(sum(2,4))
---
+++ math_1.py
@@ -0,0 +1,6 @@
+
+def sum(n1,n2):
+    print(n1 + n2)
+
+def mult(n1,n2):
+    print(n1 * n2)
